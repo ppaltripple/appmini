cdd29aa6eb3529c7c50d25d6fb52c912e0cf5188
ppaltripple <63242828+ppaltripple@users.noreply.github.com>
2020-08-26 23:48:10 +0300

    Updates

---
+++ precache-manifest.669a53b884cfcacadbd5cd5f3896af6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8431c355a87157ecbb4a",
+    "url": "./static/css/main.eeffc1e5.chunk.css"
+  },
+  {
+    "revision": "8431c355a87157ecbb4a",
+    "url": "./static/js/main.f749949a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c6a17338c4c7bf4e5d63",
+    "url": "./static/css/2.6eac2be5.chunk.css"
+  },
+  {
+    "revision": "c6a17338c4c7bf4e5d63",
+    "url": "./static/js/2.b2cb9fef.chunk.js"
+  },
+  {
+    "revision": "a6402a4a6127f6bdc5066b185bea88f3",
+    "url": "./index.html"
+  }
+];
